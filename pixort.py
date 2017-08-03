135860ca705840bde67f16bea3a33c3f6d0bd5ac
Florent Defay <spira.inhabitant@gmail.com>
2017-08-03 22:14:13 +0200

    Operational

--- pixort.py
+++ pixort.py
@@ -2,29 +2,34 @@
 
 import cv2
 import os
+import shutil
+import errno
 from collections import deque
-
-options = {
-        1048683: 'keep',
-        1048674: 'best',
-        1048676: 'delete',
-        1048693: 'undecided',
-        }
 
 WIN_HEIGHT = 720
 WIN_WIDTH = 1280
+
+options = [
+    { 'key': 1048683, 'act': 'mv', 'dir': 'keep' },
+    { 'key': 1048674, 'act': 'mv', 'dir': 'best' },
+    { 'key': 1048676, 'act': 'mv', 'dir': 'delete' },
+    { 'key': 1048693, 'act': 'mv', 'dir': 'undecided' },
+    { 'key': 1048690, 'act': 'mv', 'dir': 'retouche' },
+    { 'key': 1048685, 'act': 'cp', 'dir': 'misc' },
+]
 
 def fetch(pic):
     if pic['img'] == None:
         pic['img'] = cv2.imread(pic['fileName'])
 
+
 def show(pic):
     cv2.imshow('pic_win', pic['img'])
+    print(pic['fileName'])
 
 
 def nextPicture(current, pictures):
     if current != None:
-        print 'push'
         pictures.append(current)
 
     try:
@@ -44,7 +49,7 @@ def previousPicture(current, pictures):
 
     try:
         current = pictures.pop()
-    except StopIteration:
+    except IndexError:
         return None
 
     fetch(current)
@@ -52,35 +57,90 @@ def previousPicture(current, pictures):
 
     return current
 
+
+def moveToDirectory(current, pictures, dir):
+    if current == None:
+        return None
+
+    try:
+        os.makedirs(dir)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dir):
+            pass
+        else:
+            raise
+
+    base = os.path.splitext(current['fileName'])
+    for fileName in os.listdir('.'):
+        if fileName.startswith(base):
+            os.rename(fileName, dir + '/' + fileName)
+            print('moved picture to ' + dir)
+
+    return nextPicture(None, pictures)
+
+
+def copyToDirectory(current, pictures, dir):
+    if current == None:
+        return None
+    
+    try:
+        os.makedirs(dir)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dir):
+            pass
+        else:
+            raise
+
+    base = os.path.splitext(current['fileName'])
+    for fileName in os.listdir('.'):
+        if fileName.startswith(base):
+            shutil.copy2(fileName, dir + '/' + fileName)
+            print('copied picture to ' + dir)
+
+    return nextPicture(None, pictures)
+
+
+
+# Here it begins ...
+
 pictures = deque()
 
-for fileName in os.listdir('.'):
+for fileName in sorted(os.listdir('.')):
     if any(fileName.endswith(ext) for ext in ['jpg', 'jpeg', 'JPG', 'JPEG']):
         pictures.append({ 'fileName': fileName,
                           'img': None })
-print pictures
-
-current = None
+print(pictures)
 
 cv2.namedWindow('pic_win', cv2.WINDOW_NORMAL);
 cv2.resizeWindow('pic_win', WIN_WIDTH, WIN_HEIGHT)
 
+current = nextPicture(None, pictures)
+
 while True:
+    if current == None:
+        print('all done ;)')
+        break
     key = cv2.waitKey(0)
 
     if key == 1048603: # ESC
         break
-    elif key in options.keys():
-        print options[key]
     elif key == 1113939: # Right
-        print 'next'
+        print('next')
         current = nextPicture(current, pictures)
     elif key == 1113937: # Left
-        print 'previous'
+        print('previous')
         current = previousPicture(current, pictures)
+    elif any(opt['key'] == key for opt in options):
+        opt = next((opt for opt in options if opt['key'] == key), None)
+        if opt['act'] == 'mv':
+	    current = moveToDirectory(current, pictures, opt['dir'])
+        elif opt['act'] == 'cp':
+	    current = copyToDirectory(current, pictures, opt['dir'])
     else:
-        print key
+        print('wrong command:')
+        print(key)
 
 cv2.destroyAllWindows()
 
 
+# vim: set ts=4 sw=4 expandtab
